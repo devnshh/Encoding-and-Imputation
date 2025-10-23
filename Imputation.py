7d2dfc24671fe729282312c22c99df834c9e480a
Devansh <slurpdup@protonmail.com>
2025-10-23 16:15:02 +0530

    using pandas

--- Imputation.py
+++ Imputation.py
@@ -8,25 +8,9 @@ data_1 = {
         'Satisfaction': ['Higher', 'Lower', 'Higher', 'Medium', 'Medium', 'Lower', 'Medium', 'Lower'],
     }
 
-mileages_notMissing = []
-for mileage in data_1['Mileage']:
-    if not math.isnan(mileage):
-        mileages_notMissing.append(mileage)
-
-meanMileage = sum(mileages_notMissing) / len(mileages_notMissing)
-
-imputedMileages = []
-for mileage in data_1['Mileage']:
-    if math.isnan(mileage):
-        imputedMileages.append(meanMileage)
-    else:
-        imputedMileages.append(mileage)
-
-data_1['Mileage'] = imputedMileages
+dataframe = pd.DataFrame(data_1)
+meanMileage = dataframe['Mileage'].mean()
+dataframe['Mileage'] = dataframe['Mileage'].fillna(meanMileage)
 
 print("Imputed data:")
-numberOfCars = len(data_1['Car_Manufacturer'])
-for i in range(numberOfCars):
-    car = data_1['Car_Manufacturer'][i]
-    mileage = data_1['Mileage'][i]
-    print(f"{car}: {mileage:.1f}")
+print(dataframe)
